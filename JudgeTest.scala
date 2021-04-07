0358b514e2e03969301e49ddf67006be3ccd1d54
KrisMinchev <kristian.minchev@chch.ox.ac.uk>
2021-04-07 18:22:51 +0300

    Added comments

--- JudgeTest.scala
+++ JudgeTest.scala
@@ -3,8 +3,8 @@ import java.io.File
 
 object Judge{
 
-    def compileScala(problem: String, name: String): Boolean=
-    {
+    def compileScala(problem: String, name: String): Boolean={
+        
         // Create a "fsc filename.scala" process for the command prompt
         val compile = Seq("fsc", name)
 
@@ -24,6 +24,7 @@ object Judge{
     }
 
     def runScala(problem: String, name: String, inputFile: String, outputFile: String): String ={
+
         // Note: All codes which are to be tested should be objects which name is Solution
         // Create a "fsc filename.scala" process for the command prompt
         val run = Seq("scala", "Solution")
@@ -48,10 +49,8 @@ object Judge{
     }
 
     def judgeScala(problem: String, name: String, tests: Int): String={
+
         //Combining compileScala and runScala functions
-        //CE means compilation was unsuccessful
-        //OK means all tests are passed
-        //WA means one of the tests didn't pass
         if(compileScala(problem, name))
         {
             var i = 1
@@ -66,19 +65,19 @@ object Judge{
         else return "CE"
     }
 
-    def compileCpp(problem: String, name: String): Boolean=
-    {
-        //Create a "g++ -o program filename.cpp" process for the command prompt
+    def compileCpp(problem: String, name: String): Boolean={
+
+        // Create a "g++ -o program filename.cpp" process for the command prompt
         val compile = Seq("g++", "-o", "program", name)
 
-        //The "g++" command won't be recognized on windows if that piece of code is not included
+        // The "g++" command won't be recognized on windows if that piece of code is not included
         val os = sys.props("os.name").toLowerCase
         val panderToWindows = os match {
             case x if x contains "windows" => Seq("cmd", "/C") ++ compile
             case _ => compile
         }
 
-        //Run the process
+        // Run the process
         panderToWindows.!
 
         // Check if the compilation was successful by checking if a compiled file is present
@@ -87,6 +86,7 @@ object Judge{
     }
 
     def runCpp(problem: String, name: String, inputFile: String, outputFile: String): String ={
+
         // Note: All codes which are to be tested should be objects which name is Solution
         // Create a "fsc filename.scala" process for the command prompt
         val run = Seq(raw".\program.exe")
@@ -95,24 +95,24 @@ object Judge{
         val inp = Process.cat(new File(inputFile))
         val out = Process.cat(new File(outputFile))
 
-        //The running command may not be recognized on windows if that piece of code is not included
+        // The running command may not be recognized on windows if that piece of code is not included
         val os = sys.props("os.name").toLowerCase
         val panderToWindows1 = os match {
             case x if x contains "windows" => Seq("cmd", "/C") ++ run
             case _ => run
         }
 
-        //Piping the input to the compiled program
+        // Piping the input to the compiled program
         val result = (inp #> panderToWindows1).!!
 
-        //Checking whether the result of the program is equal to the desired output
+        // Checking whether the result of the program is equal to the desired output
         if(out.!! == result) return "OK"
         else return "WA"
     }
 
     def judgeCpp(problem: String, name: String, tests: Int): String={
 
-        //Combining compileCpp and runCpp functions
+        // Combining compileCpp and runCpp functions
         if(compileCpp(problem, name))
         {
             var i = 1
